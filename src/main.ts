bcae5da46b68008557e7a5c4a697e2a7c9ffeda8
DrxcoDev2 <drxcofn2023@gmail.com>
2025-03-02 16:34:40 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
-import {app, BrowserWindow } from "electron";
-import noti from "./utils/notification";
+import { app, BrowserWindow } from "electron";
+import path from "path";
 
-let mainWindow: BrowserWindow | null;
+let mainWindow: BrowserWindow | null = null;
 
 app.whenReady().then(() => {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 700,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: __dirname + "/preload.js"
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: true, // Habilita el uso de IPC en el renderizador
+            webviewTag: true, // Aseg�rate de habilitar el tag <webview>
         }
     });
 
+    // Carga el archivo index.html
     mainWindow.loadFile("index.html");
 
+    // Maneja el cierre de la ventana correctamente
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
 });
