b4bc188ce5f4811b3a5011f64ed06782dce1fd9d
DrxcoDev2 <drxcofn2023@gmail.com>
2025-03-04 12:02:35 +0100

    add capacitador

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.paradigma.app',
+  appName: 'paradigma',
+  webDir: 'dist',
+  
+};
+
+export default config;
