8b61032371a821a0039bf23b8114ae333299bd95
DrxcoDev2 <drxcofn2023@gmail.com>
2025-03-04 12:45:24 +0100

    commit

--- src/utils/notification.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const electron_1 = require("electron");
-const noti = new electron_1.Notification({
-    title: "Hola",
-    body: "Bienvenid@ a Paradigma"
-}).show();
-module.exports = noti;
